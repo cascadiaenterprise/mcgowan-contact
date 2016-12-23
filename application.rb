2d8a95096f7eeb9f76a45dcc7c498e91b11ff20b
Dan McGowan <dmcgowan@partner-path.com>
2016-12-23 08:02:16 -0800

    testing post params

--- application.rb
+++ application.rb
@@ -32,7 +32,7 @@ post '/send_email' do
       { :message => 'failure_email' }.to_json
     end
   else
-    { :message => "#{params[:email]} is unauthorized." }.to_json
+    { :message => "#{params} is unauthorized." }.to_json
   end
 end
 
